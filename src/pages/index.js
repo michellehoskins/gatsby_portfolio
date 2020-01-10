ce7b22767e725eb390d30fa302c4a52b8d834d63
MM Williams <mmwilliamscodes@gmail.com>
2020-01-10 18:26:35 -0500

    correct home page with links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,26 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  render(){
+      return(
+        <Layout>
+           <SEO title="Home" />
+          <div id="bg-img">
+            <main id="home" >
+              <h1 className="lg-heading" id="myName">
+                Michelle <span className="text-secondary">Williams</span>
+              </h1>
+              <h2 className="sm-heading">
+                Web Developer
+              </h2>
+              </main>
+          </div>
+          </Layout>
+      )
+  }
+}
 
 export default IndexPage
