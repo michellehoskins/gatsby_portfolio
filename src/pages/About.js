c7a00f58dd5850c50795caab515c399c281c0854
MM Williams <mmwilliamscodes@gmail.com>
2020-08-12 22:25:22 -0400

    updated jobs

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,55 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 import Layout from "../components/layout"
-import Photo2 from '../images/moi2.jpg';
+import Photo2 from "../images/moi2.jpg"
 
 class About extends Component {
-    render(){
-        return(
-        <Layout>
-            <div id="bg-img">
-              <main id="about">
-                <h1 className="lg-heading">About <span className="text-secondary">Me</span></h1>
-                <h2 className="sm-heading">
-                  A little about me
-                </h2>
-              <div className="about-info">
-                <img src={Photo2} alt="Michelle Williams" className="bio-image" />
+  render() {
+    return (
+      <Layout>
+        <div id="bg-img">
+          <main id="about">
+            <h1 className="lg-heading">
+              About <span className="text-secondary">Me</span>
+            </h1>
+            <h2 className="sm-heading">A little about me</h2>
+            <div className="about-info">
+              <img src={Photo2} alt="Michelle Williams" className="bio-image" />
               <div className="bio">
-                <h3 className="text-secondary">
-                  Biography
-                </h3>
+                <h3 className="text-secondary">Biography</h3>
                 <p>
-                  Accomplished in Web and Software Development: HTML, CSS, SASS, Javascript, ES6+, AJAX, APIs, PHP, MySQL, Node.js, Express, React, MongoDB, Laravel, CakePHP, WordPress, HTML Emails, GitHub, BitBucket, Adobe Creative Suite, and Version Control.
+                  Accomplished in Web and Software Development: HTML, CSS, SASS,
+                  Javascript, ES6+, AJAX, APIs, PHP, MySQL, Node.js, Express,
+                  React, MongoDB, Laravel, CakePHP, WordPress, HTML Emails,
+                  GitHub, BitBucket, Adobe Creative Suite, and Version Control.
                 </p>
               </div>
 
-        <div className="job job-1">
-          <h3>Case Western Reserve University</h3>
-          <h6>Full Stack Web Developer Bootcamp</h6>
-          <p>
-            It was a 6 month development bootcamp. We received a solid background in HTML, CSS, and Javascript before diving into the MERN stack. Built real projects with real people.
-          </p>
-        </div>
-        <div className="job job-2">
-          <h3>my360e</h3>
-          <h6>Developer</h6>
-          <p>
-            Created enterprise level software for contractors. Built with the CakePHP framework.
-          </p>
-        </div>
-        <div className="job job-3">
-          <h3>Get On Health</h3>
-          <h6>Developer</h6>
-          <p>
-            Created a beautiful application for MDs and other healthcare professionials who want Telehealth services for their patients.
-          </p>
-        </div>
-      </div>
-    </main>
+              <div className="job job-1">
+                <h3>my360e</h3>
+                <h6>Software Engineer</h6>
+                <p>
+                  Created enterprise level software for contractors. Built with
+                  the CakePHP framework.
+                </p>
+              </div>
+              <div className="job job-2">
+                <h3>Generator Works</h3>
+                <h6>Web Developer</h6>
+                <p>
+                  Built applications to enable telehealth for healthcare
+                  providers and their patients.
+                </p>
+              </div>
+              <div className="job job-3">
+                <h3>BoxCast</h3>
+                <h6>Software Engineer</h6>
+                <p>Delivering live-streaming services to clients.</p>
+              </div>
             </div>
-            </Layout>
-        )
-    }
+          </main>
+        </div>
+      </Layout>
+    )
+  }
 }
 
-export default About;
+export default About
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,53 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 import Layout from "../components/layout"
 
 class Contact extends Component {
-    render(){
-        return(
-          <Layout>
-            <div id="bg-img">
-              <main id="contact">
-                <h1 className="lg-heading">Contact <span className="text-secondary">Me</span></h1>
-                <h2 className="sm-heading">
-                  Send me a message
-                </h2>
-              <div className="contact-form">
-                
+  render() {
+    return (
+      <Layout>
+        <div id="bg-img">
+          <main id="contact">
+            <h1 className="lg-heading">
+              Contact <span className="text-secondary">Me</span>
+            </h1>
+            <h2 className="sm-heading">Send me a message</h2>
+            <div className="contact-form">
               <div className="bio">
                 <h3 className="text-secondary">
-                  <form method="POST" data-netlify="true" netlify-honeypot="bot-field" name="contact" className="myform">
-                  <input type="hidden" name="form-name" value="contact"/>
+                  <form
+                    method="POST"
+                    data-netlify="true"
+                    netlify-honeypot="bot-field"
+                    name="contact"
+                    className="myform"
+                  >
+                    <input type="hidden" name="form-name" value="contact" />
                     <input type="hidden" name="bot-field" />
                     <div className="fields">
-                        <label>Name
-                        <input type="text" name="name" placeholder="John Doe" />
-                        </label>
+                      <label>Name</label>
+                      <input type="text" name="name" placeholder="John Doe" />
                     </div>
 
                     <div className="fields">
-                        <label>Email
-                        <input type="email" name="email" placeholder="example@gmail.com" />
-                        </label>
+                      <label>Email</label>
+                      <input
+                        type="email"
+                        name="email"
+                        placeholder="example@gmail.com"
+                      />
                     </div>
 
                     <div className="fields">
-                        <label>Message
-                        <textarea name="message"></textarea>
-                        </label>
+                      <label>Message</label>
+                      <textarea name="message"></textarea>
                     </div>
-                
-                    <button className="button" name="submit" type="submit">Submit</button>
+
+                    <button className="button" name="submit" type="submit">
+                      Submit
+                    </button>
                   </form>
                 </h3>
- 
               </div>
-
-      </div>
-    </main>
             </div>
-            </Layout>
-        )
-    }
+          </main>
+        </div>
+      </Layout>
+    )
+  }
 }
 
-export default Contact;
+export default Contact
